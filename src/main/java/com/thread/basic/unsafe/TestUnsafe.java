7e8fc3f015079124b9a311d7a85b162a0a156c19
yangguang <yangguang@qq.com>
2022-01-09 18:07:56 +0800

    CAS操作以及Unsafe类的使用

---
+++ src/main/java/com/thread/basic/unsafe/TestUnsafe.java
@@ -0,0 +1,38 @@
+package com.thread.basic.unsafe;
+
+import sun.misc.Unsafe;
+
+/**
+ *  不能通过正规渠道使用Unsafe这个类。因为这个类是直接操作内存的对象。不安全。
+ *  所以Unsafe类在设计的时候，不能从外界直接使用。
+ *  但是可以使用间接的方法使用:
+ *      1:使用反射。
+ */
+public class TestUnsafe {
+    //获取Unsafe的实例
+    static final Unsafe unsafe = Unsafe.getUnsafe();
+
+    //记录变量state在类TestUnsafe中的偏移值
+    static final long stateOffset;
+
+
+    private volatile long state = 0;
+
+    static {
+        try {
+            //获取state变量在类TestUnsafe中的偏移值.
+            stateOffset = unsafe.objectFieldOffset(TestUnsafe.class.getDeclaredField("state"));
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+            throw new Error(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        TestUnsafe testUnsafe = new TestUnsafe();
+        //如果testUnsafe对象中内存偏移量为stateOffset的state变量的值为0,就更新该值为1.
+        boolean b = unsafe.compareAndSwapInt(testUnsafe, stateOffset, 0, 1);
+        System.out.println(b);
+
+    }
+}
